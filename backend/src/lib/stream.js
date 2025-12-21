5480637acce4ce1c7e34b525ca993d4d40bd156d
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-12-21 21:16:49 +0530

    auth and stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,30 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret) {
+    throw new Error("Stream API key and secret is missing.");
+    console.error("Stream API key and secret is missing.");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await chatClient.upsertUser(userData);
+        console.log("User Upserted Successfully", userData);
+    } catch (error) {
+        console.error("Error upserting Stream user:", error);
+    }
+}
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await chatClient.deleteUser(userId);
+        console.log("User Deleted Successfully");
+    } catch (error) {
+        console.error("Error deleting Stream user:", error);
+    }
+}

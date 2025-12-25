58d0783478501ae67f574faae71698bbcf26a67e
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-12-25 19:42:25 +0530

    feat: add HomePage and AboutPage components, implement conditional routing for problem page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
-import { Route, Routes } from 'react-router'
+import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
+import { Navigate, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
 
 function App() {
-
+  
+  const { isSignedIn } = useUser();
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/about' element={<AboutPage />} />
+      <Route path='/problem' element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />} />
     </Routes>
   )
 }
